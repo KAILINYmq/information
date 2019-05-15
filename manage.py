0b7d10a42b9c2f0c2482856ced7b97e495ee88cd
KAILIN <2534992864@qq.com>
2019-05-15 13:07:35 +0800

    Session的保存位置指定&测试

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
-from flask import  Flask
+from flask import  Flask,session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session  # 可以指定 session 保存的位置
 
 """项目入口文件"""
 class Config(object):
     """项目配置"""
     DEBUG = True
+    SECRET_KEY = "DvekNWw/tf8R2I9vfdQJx0HvNhCauYb+hiqv/UI6mEEQ8qfRYx96IQDBX65B9OAM"
 
     # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@127.0.0.1:3306/information"
@@ -14,8 +16,19 @@ class Config(object):
 
     # Redis的配置
     REDIS_HOST = "127.0.0.1"
-    REDIS_PASSWORD = "123456"
     REDIS_PORT = 6379
+
+    # Session保存位置
+    SESSION_TYPE = "redis"
+    # 开启 session 签名
+    SESSION_USE_SIGER = True
+    # 指定 Session 保存 redis
+    SEESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 
 app = Flask(__name__)
 # 加载配置
@@ -24,12 +37,15 @@ app.config.from_object(Config)
 # 初始化数据库
 db = SQLAlchemy(app)
 # 初始化 redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, password=Config.REDIS_PASSWORD , port=Config.REDIS_PORT)
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启项目 CSRF 保护, 只做服务器验证功能
 CSRFProtect(app)
+# 设置session保存指定位置
+Session(app)
 
 @app.route('/')
 def index():
+    session["name"] = "kailin"
     return "index"
 
 if __name__ == "__main__":
