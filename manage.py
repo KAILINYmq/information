c66f8cc0e4ebe4ed8e7ddfd1e276288aa82f3d6b
KAILIN <2534992864@qq.com>
2019-05-15 08:34:13 +0800

    项目配置&数据库配置

--- manage.py
+++ manage.py
@@ -1,10 +1,24 @@
 from flask import  Flask
+from flask_sqlalchemy import SQLAlchemy
+
+"""项目入口文件"""
+class Config(object):
+    """项目配置"""
+    DEBUG = True
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
-    return "indexsss"
+    return "index"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
