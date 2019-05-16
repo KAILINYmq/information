ba4e1364d102ad51c8e2f190c74902e81069f79e
KAILIN <2534992864@qq.com>
2019-05-16 14:04:09 +0800

    根路径模板渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,8 @@
+from flask import render_template
 from info import redis_store
 from . import index_blu
 
 @index_blu.route('/')
 def index():
-    # 向 redis 中保存一个值 name itcast
-    redis_store.set("name","KAILIN")
+    return  render_template('news/index.html')
 
-    return "index"
