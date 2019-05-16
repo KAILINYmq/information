6f4fad8aea52434cc76a6cdbdfae59402c28f75f
KAILIN <2534992864@qq.com>
2019-05-16 18:17:40 +0800

    加载favicon.ico图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 from info import redis_store
 from . import index_blu
 
@@ -6,3 +6,8 @@ from . import index_blu
 def index():
     return  render_template('news/index.html')
 
+# 打开网页时，浏览器默认去请求根路径+favicon.ico作为网站标签的小图标
+# send_static_file是 flask 去查找指定的静态文件所调用的方法
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/xds.ico')
