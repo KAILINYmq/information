5ed0f4ba043fcd0a21fc591571f48f6797c70d91
KAILIN <2534992864@qq.com>
2019-05-19 11:27:02 +0800

    收藏和取消收藏后端逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,11 +1,56 @@
+from flask.json import jsonify
 from info import constants
 from info.models import News
 from info.modules.news import news_blu
-from flask import render_template, current_app, session, g, abort
+from flask import render_template, current_app, session, g, abort, request
 from info.untils.common import user_login_data
+from info.untils.response_code import RET
 
 
-@news_blu.route('/<int:news_id>')
+@news_blu.route("/news_collect", methods=["POST"])
+@user_login_data
+def collect_news():
+    """收藏新闻"""
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1. 接收参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+    # 2. 判断参数
+    if not ([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if action not in ["collect", "cancel_collect"]:
+        return  jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="参数错误")
+    # 3. 查询新闻，判断是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="为查询到数据")
+
+    # 4.收藏和取消收藏
+    if action == "cancel_collect":
+        # 取消收藏
+        if news in user.collection_news:
+            # 添加到用户的新闻列表
+            user.collection_news.remove(news)
+    else:
+        if news not in  user.collection_news:
+            # 添加到用户的新闻列表
+            user.collection_news.append(news)
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
+
+@news_blu.route('/<int:news_id>' )
 @user_login_data
 def news_detail(news_id):
      """新闻详情"""
@@ -38,11 +83,16 @@ def news_detail(news_id):
 
      # 判断用户是否收藏
      is_collected = False
+     if user:
+         # 判断用户是否收藏这个新闻
+         # 如果当前新闻在用户收藏的新闻里面 user.collection_news后面可以不用加all，因为sqlalchemy会在使用的时候自动加载
+         if news in user.collection_news:
+             is_collected = True
 
      data={
          "user": user.to_dict() if user else None,  # 如果user有值执行user.to_dcit() 否则为None
          "news_dict_li": news_dict_li,              # 获取主页新闻数据
-         "news": news.to_dict()                     # 新闻详情页数据
+         "news": news.to_dict(),                    # 新闻详情页数据
          "is_collected": is_collected               # 判断用户是否收藏
      }
 
