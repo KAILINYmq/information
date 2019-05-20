b9b52547430234ae70e2133e500d8bc51d6a472b
KAILIN <2534992864@qq.com>
2019-05-20 10:57:14 +0800

    上传头像后端代码实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,16 +1,36 @@
-from flask import render_template, g, redirect, request, jsonify
+from flask import render_template, g, redirect, request, jsonify, current_app
+from info import constants
 from info.modules.profile import profile_blu
 from info.untils.common import user_login_data
+from info.untils.image_storage import storage
 from info.untils.response_code import RET
 
 @profile_blu.route('/pic_info', methods=["GET","POST"])
 @user_login_data
 def pic_info():
     """用户头像上传"""
+    user = g.user
     if request.method == "GET":
-        user = g.user
         return render_template('news/user_pic_info.html', data={"user": user.to_dict()})
 
+    # 代表用户在保存数据
+    # 1. 取到上传的图片
+    try:
+        avatar = request.files.get("avatar").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 2.上传头像
+    try:
+        # 自己封装方法上传
+        key = storage(avatar)
+    except Exception as e:
+        return jsonify(errno=RET.THIRDERR, errmsg="上传头像错误")
+
+    # 3. 保存头像地址到数据库
+    user.avatar_url = key
+    return jsonify(errno=RET.OK, errmsg="OK", avatar_url=constants.QINIU_DOMIN_PREFIX+key)
 
 
 @profile_blu.route('/base_info', methods=["GET","POST"])
